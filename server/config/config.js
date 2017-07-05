a9297f535a4c932637a6e2249ddf61bf1dd2d83e
Ilya Sheidin <ilya.sheidin@gmail.com>
2017-07-05 19:59:45 +0300

    Fix port

--- server/config/config.js
+++ server/config/config.js
@@ -3,4 +3,3 @@ const env = process.env.NODE_ENV || 'development';
 if(env === 'development'){
     process.env.MONGO_DB = 'mongodb://localhost:27017/TodoApp';
 }
-const port = process.env.PORT || 3000;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 require('./config/config.js');
+const port = process.env.PORT || 3000;
 const spdy = require('spdy');
 const express = require('express');
 const path = require('path');
