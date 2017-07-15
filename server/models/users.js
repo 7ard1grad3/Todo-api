b523ec65a572a867d50a552c092f739e89e90efb
Ilya Sheidin <ilya.sheidin@gmail.com>
2017-07-15 20:03:37 +0300

    New userts model

--- server/models/users.js
+++ server/models/users.js
@@ -1,9 +1,21 @@
-var mongoose = require('mongoose');
-var User = mongoose.model('Users',{
+const mongoose = require('mongoose');
+const validator = require('validator');
+const User = mongoose.model('Users',{
     email: {
         type: String,
         required: true,
         minlength: 1,
+        trim: true,
+        unique: true,
+        validator: (value) => {
+            return validator.isEmail(value);
+        },
+        message: '{VALUE} is not a valid email'
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
         trim: true
     }
 });
